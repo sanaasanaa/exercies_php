8c5aea5b16a6a8885dff6a274e6254dbb6baa03c
sanaa <s.rortas@codeur.online>
2020-10-26 12:14:29 +0100

    debut  bis

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
+<?php
+
+$civ=$_POST["civ"];
+if ($civ=="mr") {
+   echo "bonjour monsieur";
+}
+elseif ($civ=="mme") {
+   echo "bonjour madame";
+}
+
+
+
+
+
+?>
 <form action="index.php" method="POST" >
-<label for="c"> c </label>
-<select name="nom" >
-<option value="mr"> mr </option>
-<option value="mme"> mme </option>
+<label for="civ"> civ </label>
+<select name="civ"> 
+
+  <option value="-">-</option>
+  <option value="mr"> mr </option>
+  <option value="mme"> mme </option>
+
 </select>
+
+<label for="nom"> nom </label><br>
+<input type="text" name="nom"><br>
+
+<label for="prenom"> prenom </label><br>
+<input type="text" name="prenom"><br>
+
+<label for="adresse"> adresse </label><br>
+<input type="text" name="adresse"><br>
+
+
+
+
+
+
+<input type="submit" name="envoyer">
 </form>
